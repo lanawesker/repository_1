8122cde8bc6381bad6b7ea311af6ef53951fb99a
lanawesker <76243335+lanawesker@users.noreply.github.com>
2020-12-21 17:41:27 +0200

    Добавила домашку со второго урока.

---
+++ lesson_2.py
@@ -0,0 +1,26 @@
+# task 1
+name = "Lana"
+day = "18.12.2020"
+print("Good day,", name, "!", day, "is a perfect day to learn some python.")
+# task 2
+first_name = "Ivan"
+last_name = "Ivanov"
+user_name = first_name + last_name
+print("Have a good day,", user_name, "!")
+# task 3
+a = 5
+b = 10
+c = a + b
+print(c)
+c = a - b
+print(c)
+c = a / b
+print(c)
+c = a * b
+print(c)
+c = a ** b
+print(c)
+c = c % b
+print(c)
+c = c // b
+print(c)
