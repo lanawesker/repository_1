30b5be0bcf1176a64662a857db4019d15fb79d3d
lanawesker <76243335+lanawesker@users.noreply.github.com>
2020-12-21 20:42:27 +0200

    2

--- lesson_2.py
+++ lesson_2.py
@@ -1,16 +1,30 @@
 # task 1
+# Make a program that has your name and the current day of the week stored
+# as separate variables and then prints a message like this:
+# “Good day <name>! <day> is a perfect day to learn some python.”
+# Note that <name> and <day> are predefined variables in source code.
+# An additional bonus will be to use different string formatting methods for constructing result string.
 if __name__ == '__main__':
     name = "Lana"
     day = "18.12.2020"
     print("Good day,", name, "!", day, "is a perfect day to learn some python.")
 # task 2
-if __name__ == '__main__':
+# Save your first and last name as separate variables,
+# then use string concatenation to add them together with a white space in between and print a greeting.
     first_name = "Ivan"
     last_name = "Ivanov"
     user_name = first_name + last_name
     print("Have a good day,", user_name, "!")
 # task 3
-if __name__ == '__main__':
+# Make a program with 2 numbers saved in separate variables a and b,
+# then print the result for each of the following:
+# Addition
+# Subtraction
+# Division
+# Multiplication
+# Exponent (Power)
+# Modulus
+# Floor division
     a = 5
     b = 10
     c = a + b
