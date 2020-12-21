3da2dd3162fd0308fff24ffccaa911e9fa16feab
lanawesker <76243335+lanawesker@users.noreply.github.com>
2020-12-21 18:46:29 +0200

    if __name__ == '__main__':

--- lesson_1.py
+++ lesson_1.py
@@ -1,22 +1,23 @@
 # task 1
-print(1, 3, 5, 7, 11, sep='#')  # Ex. 1#3#5#7#11
-print(1, 3, 5, 7, 11, sep='#', end='\t')
-print(1, 3, 5, 7, 11, end='!')  # Ex. 1#3#5#7#11	1 3 5 7 11!
+if __name__ == '__main__':
+    print(1, 3, 5, 7, 11, sep='#')  # Ex. 1#3#5#7#11
+    print(1, 3, 5, 7, 11, sep='#', end='\t')
+    print(1, 3, 5, 7, 11, end='!')  # Ex. 1#3#5#7#11	1 3 5 7 11!
 # task 2
-print('#########')
-print('#', end='\t\t')
-print('#')
-print('#', end='\t\t')
-print('#')
-print('#', end='\t\t')
-print('#')
-print('#########', end='\n\n')
-print('#', end='\t\t')
-print('#')
-print('#', end='\t\t')
-print('#')
-print('#########')
-print('#', end='\t\t')
-print('#')
-print('#', end='\t\t')
-print('#')
+    print('#########')
+    print('#', end='\t\t')
+    print('#')
+    print('#', end='\t\t')
+    print('#')
+    print('#', end='\t\t')
+    print('#')
+    print('#########', end='\n\n')
+    print('#', end='\t\t')
+    print('#')
+    print('#', end='\t\t')
+    print('#')
+    print('#########')
+    print('#', end='\t\t')
+    print('#')
+    print('#', end='\t\t')
+    print('#')
