523c6239d5a3d0cdea3eebc9ee2c889624f1ae62
lanawesker <76243335+lanawesker@users.noreply.github.com>
2020-12-21 18:45:29 +0200

    if __name__ == '__main__':

--- lesson_2.py
+++ lesson_2.py
@@ -1,26 +1,27 @@
 # task 1
-name = "Lana"
-day = "18.12.2020"
-print("Good day,", name, "!", day, "is a perfect day to learn some python.")
+if __name__ == '__main__':
+    name = "Lana"
+    day = "18.12.2020"
+    print("Good day,", name, "!", day, "is a perfect day to learn some python.")
 # task 2
-first_name = "Ivan"
-last_name = "Ivanov"
-user_name = first_name + last_name
-print("Have a good day,", user_name, "!")
+    first_name = "Ivan"
+    last_name = "Ivanov"
+    user_name = first_name + last_name
+    print("Have a good day,", user_name, "!")
 # task 3
-a = 5
-b = 10
-c = a + b
-print(c)
-c = a - b
-print(c)
-c = a / b
-print(c)
-c = a * b
-print(c)
-c = a ** b
-print(c)
-c = c % b
-print(c)
-c = c // b
-print(c)
+    a = 5
+    b = 10
+    c = a + b
+    print(c)
+    c = a - b
+    print(c)
+    c = a / b
+    print(c)
+    c = a * b
+    print(c)
+    c = a ** b
+    print(c)
+    c = c % b
+    print(c)
+    c = c // b
+    print(c)
