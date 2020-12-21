d23a8eee4241ab72b8ebc13da0c7664a03fc419c
lanawesker <76243335+lanawesker@users.noreply.github.com>
2020-12-21 18:52:19 +0200

    if __name__ == '__main__':

--- lesson_2.py
+++ lesson_2.py
@@ -4,11 +4,13 @@ if __name__ == '__main__':
     day = "18.12.2020"
     print("Good day,", name, "!", day, "is a perfect day to learn some python.")
 # task 2
+if __name__ == '__main__':
     first_name = "Ivan"
     last_name = "Ivanov"
     user_name = first_name + last_name
     print("Have a good day,", user_name, "!")
 # task 3
+if __name__ == '__main__':
     a = 5
     b = 10
     c = a + b
